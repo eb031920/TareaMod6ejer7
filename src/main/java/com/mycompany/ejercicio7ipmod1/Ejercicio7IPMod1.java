2b56111ca712689151629ed6fe09071e3173a84d
Dell <Dell@DESKTOP-J5Q3ESE>
2024-11-04 21:59:54 -0600

    TareaModulo6

---
+++ src/main/java/com/mycompany/ejercicio7ipmod1/Ejercicio7IPMod1.java
@@ -0,0 +1,20 @@
+//Copyrith:Ruth Bautista
+// Ciclo for que empieza en 2 y va hasta 100, imprimiendo solo números pares.
+// i = 2 es el primer número par, i <= 100 asegura que no pasemos de 100,
+// y i += 2 incrementa de 2 en 2 para obtener solo números pares.
+package com.mycompany.ejercicio7ipmod1;
+
+
+public class Ejercicio7IPMod1 {
+    public static void main(String[] args) {
+        
+        for (int i = 2; i <= 100; i += 2) {
+            
+            System.out.println(i);
+        }
+    }        
+}
+ 
+
+
+
